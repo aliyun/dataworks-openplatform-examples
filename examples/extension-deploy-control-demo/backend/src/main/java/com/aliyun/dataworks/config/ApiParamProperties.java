d71304b786d9fc2f836d6ab560c36b371a274782
tzung-han.wth <tzung-han.wth@alibaba-inc.com>
2022-12-05 17:59:37 +0800

    feat: optimze codes

--- examples/extension-deploy-control-demo/backend/src/main/java/com/aliyun/dataworks/config/ApiParamProperties.java
+++ examples/extension-deploy-control-demo/backend/src/main/java/com/aliyun/dataworks/config/ApiParamProperties.java
@@ -2,7 +2,6 @@ package com.aliyun.dataworks.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
--- examples/meta-api-demo/frontend/vite.config.ts
+++ examples/meta-api-demo/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     open: true,
+    port: 8080,
   },
   css: {
     modules: {
